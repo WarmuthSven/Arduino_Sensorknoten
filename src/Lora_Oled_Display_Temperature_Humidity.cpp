896ac86464a550f7915a9493d5f7788f0cced482
Sven <svenwarmuth1@web.de>
2023-10-12 13:12:24 +0200

    Go back to old libraries

--- src/Lora_Oled_Display_Temperature_Humidity.cpp
+++ src/Lora_Oled_Display_Temperature_Humidity.cpp
@@ -10,7 +10,6 @@ CustomLoraNetwork loraNetwork(M0_PIN,M1_PIN);
 Adafruit_AHTX0 aht;
 sensors_event_t humidity_event, temp_event;
 float relativeHumidity = 0, temperature = 0;
-int test = 0;
 
 String rcvData;
 unsigned long lastTimeSensor = 0;
@@ -31,17 +30,17 @@ int RL = 1500;
 void setup() {
 	Serial.begin(9600);
 	Oled.begin();
-	aht.begin();
+	//aht.begin();
 	
 	Oled.setFlipMode(true);
 
 
 	//pinMode(A0, INPUT);
 
-	loraNetwork.Init(maxSamples);
+	/* loraNetwork.Init(maxSamples);
 	loraNetwork.AddDataPointer(&relativeHumidity, "Relative Humidity");
 	loraNetwork.AddDataPointer(&temperature, "Temperature");
-	loraNetwork.AddDataPointer(&test, "Test");
+	loraNetwork.AddDataPointer(&test, "Test"); */
 }
 
 short sentPackageSize = 0;
@@ -58,22 +57,18 @@ int testCounter = 0;
 void loop() {
 	unsigned long curTime = millis();
 
-	loraNetwork.Update();
+	//loraNetwork.Update();
 	/*
 	snrValue = lora.RequestSNR();
 	ambientNoise = lora.GetLastNoiseStrength();
 	*/
-	if(curTime - lastTimeOLED >= 1000){		
-		if(freeRAM()>20){
-			byte* test = new byte[1];
-			test[0] = 0xFE;
-		}
+	if(curTime - lastTimeOLED >= 5000){		
 		
 		lastTimeOLED = curTime;
 
-		packageEnum = loraNetwork.lastPackageID;
+		/* packageEnum = loraNetwork.lastPackageID;
 		rssiStrength = loraNetwork.GetLastRSSI();
-
+ */
 		Oled.clearDisplay();
 
 		Oled.setFont(u8x8_font_chroma48medium8_r);
@@ -81,12 +76,12 @@ void loop() {
 		Oled.print("Ram: " + String(freeRAM()));
 		Oled.setCursor(0, 1);
 		Oled.print("rec ");
-		Oled.print(loraNetwork.recMessageCounter);
+		/* Oled.print(loraNetwork.recMessageCounter);
 		Oled.print(", sen ");
 		Oled.print(loraNetwork.sentMessageCounter);
 		Oled.setCursor(0, 2);
 		Oled.print("Address ");
-		Oled.print(loraNetwork.nodeAddress); 
+		Oled.print(loraNetwork.nodeAddress);  */
 
 		/*Oled.print(rssiStrength);
 		Oled.print(" RSSI"); */
@@ -97,25 +92,25 @@ void loop() {
 		Oled.print("RS = ");
 		Oled.print(RS); */
 
-		String data = loraNetwork.GetData(samplesCounter);
+		//String data = loraNetwork.GetData(samplesCounter);
 		Oled.setCursor(0, 3);
 		Oled.print("State: ");
-		Oled.print(loraNetwork.currentNetworkState);
+		/* Oled.print(loraNetwork.currentNetworkState);
 		//Oled.print("ID: Rec/Sent");
 		Oled.setCursor(0, 4);
 		Oled.print("rec ");
 		Oled.print(loraNetwork.receivedID);
 		Oled.setCursor(0, 5);
 		Oled.print("sen ");
-		Oled.print(loraNetwork.ID);
+		Oled.print(loraNetwork.ID); */
 		Oled.setCursor(0, 6);
 		Oled.print("Enum: ");
 		Oled.print(packageEnum);
 		Oled.setCursor(0, 7);
 		Oled.print("Size: ");
-		Oled.print(String(loraNetwork.GetLastReceivingPackageSize()));
+		/* Oled.print(String(loraNetwork.GetLastReceivingPackageSize()));
 		Oled.print("/");
-		Oled.print(String(loraNetwork.GetLastSendingPackageSize()));
+		Oled.print(String(loraNetwork.GetLastSendingPackageSize())); */
 
 		/*
 		Oled.setCursor(0, 3);
@@ -158,17 +153,15 @@ void loop() {
 
 	
 
-	if(loraNetwork.CustomDataRequested){//curTime - lastTimeSensor >= 1000){
+	/* if(loraNetwork.CustomDataRequested){//curTime - lastTimeSensor >= 1000){
 		Oled.setCursor(0,5);
 		Oled.print("GO DATA!");
 		lastTimeSensor = curTime;
 		bool gotEvents = aht.getEvent(&humidity_event, &temp_event);
 		if(gotEvents){
 			loraNetwork.CustomDataAvailable = true;
-			test%=10;
-			test +=1;
 			relativeHumidity = humidity_event.relative_humidity;
 			temperature = temp_event.temperature;
 		};
-	}
+	} */
 }
