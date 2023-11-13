68d695f0c38b18b94ff9f33afec85cfce8f58964
Sven <svenwarmuth1@web.de>
2023-11-13 14:24:23 +0100

    Cleanup Code

--- src/Lora_Oled_Display_Temperature_Humidity.cpp
+++ src/Lora_Oled_Display_Temperature_Humidity.cpp
@@ -17,10 +17,7 @@ CustomLoraNetwork loraNetwork(M0_PIN,M1_PIN);
 float pressure = 0, temperature = 0, humidity = 0;
 unsigned int co2ppm = 0, coAmount = 0, methanAmount = 0;
 
-String rcvData;
-unsigned long lastTimeSensor = 0;
 unsigned long lastTimeOLED = 0;
-bool stateChange = false;
 
 extern char  *__brkval;
 int freeRAM() {
@@ -29,15 +26,18 @@ int freeRAM() {
 
 void setup() {
 	Serial.begin(9600);
+
 	Wire.begin();
 	Wire.setWireTimeout(25000,true);
+
 	Oled.begin();
 	Oled.setFlipMode(true);
+
 	Pressure.begin();
+
 	airSensor.begin();
 	airSensor.useStaleData(true);
 	airSensor.setAltitudeCompensation(200);
-	//airSensor.setTemperatureOffset(0);
 	
 	pinMode(methanPin, INPUT);
 	pinMode(coPin, INPUT);
@@ -51,45 +51,26 @@ void setup() {
 	loraNetwork.AddDataPointer(&coAmount, "CO");
 }
 
-short sentPackageSize = 0;
-short receivedPackageSize = 0;
-short packageEnum = 0;
-short receivedRandom = 0;
-int rssiStrength = 0;
-int ambientNoise = 0;
-int snrValue = 0;
-
-int maxTests = 10;
-int testCounter = 0;
-
-
 void loop() {
 	unsigned long curTime = millis();
 
 	loraNetwork.Update();
-	/*
-	snrValue = lora.RequestSNR();
-	ambientNoise = lora.GetLastNoiseStrength();
-	*/
+
 	if(curTime - lastTimeOLED >= 2000){		
 		
 		lastTimeOLED = curTime;
 
-		/* packageEnum = loraNetwork.lastPackageID;
-		rssiStrength = loraNetwork.GetLastRSSI();
- 		*/
-
 		if(airSensor.dataAvailable()){
 			co2ppm = airSensor.getCO2();
-			temperature = airSensor.getTemperature();
 			humidity = airSensor.getHumidity();
 		}
 
 		pressure = Pressure.readPressure();
+		temperature = Pressure.readTemperature();
 
 		Oled.clearDisplay();
-
 		Oled.setFont(u8x8_font_chroma48medium8_r);
+
 		Oled.setCursor(0, 0);
 		Oled.print(F("Ram: "));
 		Oled.print(freeRAM());
@@ -98,138 +79,48 @@ void loop() {
 		Oled.print(F("CO2: "));
 		Oled.print(co2ppm); 
 		Oled.print(F(" ppm"));
+
 		Oled.setCursor(0, 2);
 		Oled.print(F("Temp: "));
 		Oled.print(temperature);
 		Oled.print(F(" C"));
+
 		Oled.setCursor(0, 3);
 		Oled.print(F("Hum: "));
 		Oled.print(humidity);
 		Oled.print(F(" %"));
+
 		Oled.setCursor(0, 4);
 		Oled.print(F("Pres: "));
 		Oled.print(pressure/100000.0);
 		Oled.print(F(" Bar"));
+
 		Oled.setCursor(0, 5);
 		Oled.print(F("Methan: "));
 		float gas = analogRead(methanPin);
 		Oled.print(gas * 100 / 1023.0);
 		Oled.print(F(" %"));
+
 		Oled.setCursor(0, 6);
 		Oled.print(F("CO: "));
 		gas = analogRead(coPin);
 		Oled.print(gas * 100 / 1023.0);
 		Oled.print(F(" %"));
-		
 
-		/*Oled.setCursor(0, 1);
-		Oled.print(F("MID: "));
-		Oled.print(loraNetwork.lastMainUnitID);
-		Oled.setCursor(0, 2);
-		Oled.print(F("ID: "));
-		Oled.print(loraNetwork.ID);
-		Oled.setCursor(0, 3);
-		Oled.print(F("Add: "));
-		Oled.print(loraNetwork.nodeAddress); 
-		Oled.setCursor(0, 4);
-		Oled.print(F("PID: "));
-		Oled.print(loraNetwork.parentID);
-		Oled.setCursor(0, 5);
-		Oled.print(F("PAdd: "));
-		Oled.print(loraNetwork.parentAddress);
-		Oled.setCursor(0, 6);
-		Oled.print(F("CRC: "));
-		Oled.print(loraNetwork.setupChecksum);*/
-
-		/* Oled.setCursor(0, 6);
-		aht.getEvent(&humidity_event, &temp_event);
-		Oled.print("Temp2: " + String(temp_event.temperature) + "°C");
-		Oled.setCursor(0, 7);
-		Oled.print("Hum2: " + String(humidity_event.relative_humidity) + "%"); */
-		/*
-		Oled.setCursor(0, 1);
-		Oled.print("rec "); 
-		Oled.print(loraNetwork.recMessageCounter);
-		Oled.print(", sen ");
-		Oled.print(loraNetwork.sentMessageCounter);
-		Oled.setCursor(0, 2);
-		Oled.print("Address ");
-		Oled.print(loraNetwork.nodeAddress);  */
-
-		/*Oled.print(rssiStrength);
-		Oled.print(" RSSI"); */
-
-		
-		/* float ADCval = analogRead(A0);
-		float VRL = (5.0 / 1023) * ADCval;
-		float RS = (VC / VRL - 1) * RL;
-		Oled.print("RS = ");
-		Oled.print(RS); */
-
-		//String data = loraNetwork.GetData(samplesCounter);
-		/*Oled.setCursor(0, 3);
-		Oled.print("State: ");
-		 Oled.print(loraNetwork.currentNetworkState);
-		//Oled.print("ID: Rec/Sent");
-		Oled.setCursor(0, 4);
-		Oled.print("rec ");
-		Oled.print(loraNetwork.receivedID);
-		Oled.setCursor(0, 5);
-		Oled.print("sen ");
-		Oled.print(loraNetwork.ID); 
-		Oled.setCursor(0, 6);
-		Oled.print("Enum: ");
-		Oled.print(packageEnum);
-		Oled.setCursor(0, 7);
-		Oled.print("Size: ");
-		Oled.print(String(loraNetwork.GetLastReceivingPackageSize()));
-		Oled.print("/");
-		Oled.print(String(loraNetwork.GetLastSendingPackageSize())); */
-
-		/*
-		Oled.setCursor(0, 3);
-		Oled.print("Real Storage:");
-		Oled.setCursor(0, 4);
-		Oled.print(data);
-
-		String recData = loraNetwork.GetRecData(samplesCounter);
-		Oled.setCursor(0, 6);
-		Oled.print("Received:");
-		Oled.setCursor(0, 7);
-		Oled.print(recData); 
-		
-		Oled.setCursor(0, 6);
-		Oled.print("Sen/Rec Size:");
-		Oled.setCursor(0, 7);
-		Oled.print(String(loraNetwork.GetLastSendingPackageSize()) + " / " + String(loraNetwork.GetLastReceivingPackageSize())); 
-		*/
-
-		/*Oled.setCursor(0, 3);
-		Oled.print("Received Random:");
-		Oled.setCursor(0, 4);
-		Oled.print(receivedRandom);
-
-		Oled.setCursor(0, 6);
-		Oled.print("RSSI/Noise/SNR: ");
-		Oled.setCursor(0, 7);
-		Oled.print(rssiStrength);
-		Oled.print(" / ");
-		Oled.print(ambientNoise);
-		Oled.print(" / ");
-		Oled.print(snrValue);*/
 		Oled.refreshDisplay();
 	}
 
-	 if(loraNetwork.CustomDataRequested){//curTime - lastTimeSensor >= 1000){
+	 if(loraNetwork.CustomDataRequested){
 		Oled.setCursor(0,7);
 		Oled.print(F("GO DATA!"));
-		lastTimeSensor = curTime;
+
 		co2ppm = airSensor.getCO2();
 		humidity = airSensor.getHumidity();
 		temperature = Pressure.readTemperature();
 		pressure = Pressure.readPressure();
 		methanAmount = analogRead(methanPin);
 		coAmount = analogRead(coPin);
+
 		loraNetwork.CustomDataAvailable = true;
 	}
 }
